81579bbd5bdd032270eca9cc07fb08da9eee7b71
Luis Aviles <1812123@alu.murciaeduca.es>
2023-12-13 13:22:47 +0100

    Comprobando si se han solucionado los errores

--- database/migrations/2023_12_13_000000_add_calificacion_to_proyectos_table.php
+++ database/migrations/2023_12_13_000000_add_calificacion_to_proyectos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('proyectos', function (Blueprint $table) {
             $table->unsignedTinyInteger('calificacion')->nullable();
+            $table->unsignedTinyInteger('calificacion')->nullable();
         });
     }
 
